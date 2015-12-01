a406013b30e5433d4628cab1ef5206284489f4b7
johns6971 <johns6971@HRH-IC0027347.wrdsb.ca>
2015-12-01 10:43:11 -0500

    Initial Commit

---
+++ src/Pet.java
@@ -0,0 +1,25 @@
+/**
+ * Represents a pet animal.
+ * @author johns6971
+ */
+public abstract class Pet {
+    private String name;
+    private int age;
+    private String gender;
+    
+    public Pet(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    
+    /**
+     * Asks the pet to speak.
+     */
+    public abstract void speak();
+    
+    /**
+     * asks the pet to move.
+     */
+    public abstract void move();
+}
